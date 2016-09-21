b1bfaa23c083c2444f8a20fc9cb2e4129486198c
Justin Doak <justinhdoak@gmail.com>
2016-09-20 21:31:44 -0400

    More formatting

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+fadeElements = [
+  "#main-head div, #main-head img",
+  "#main-landing h2",
+  "#main-landing h3",
+  "#main-landing h4, #contact a"
+];
+
+var delayedFade = function(item, time) {
+  $(item).fadeIn(time);
+};
+
+$( document ).ready( function() {
+  for(var i = 0; i<fadeElements.length; i++) {
+    delayedFade(fadeElements[i], (i+2)*1000)
+  };
+});
